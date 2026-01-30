9d79554470626b843102954a0328c284ea540446
ingalls <nick@ingalls.ca>
2026-01-30 08:26:46 -0700

    Update Task

--- task.ts
+++ task.ts
@@ -1,24 +1,20 @@
 import { Static, Type, TSchema } from '@sinclair/typebox';
 import type { Event } from '@tak-ps/etl';
 import { Feature } from '@tak-ps/node-cot'
-import ETL, { SchemaType, fetch, handler as internal, local, DataFlowType, InvocationType } from '@tak-ps/etl';
+import ETL, { SchemaType, handler as internal, local, DataFlowType, InvocationType } from '@tak-ps/etl';
+import Schema from '@openaddresses/batch-schema';
 
 const InputSchema = Type.Object({
-    API_URL: Type.String({
-        description: 'The URL of the API to fetch data from (Typically ends with /Production)'
-    }),
-    API_Token: Type.String({
-        description: 'The API token for authentication'
-    }),
     DataType: Type.String({
-        enum: ['incidents', 'units'],
-        default: 'incidents',
-        description: 'The type of data to fetch'
+        enum: [
+            'incidents',
+            'units'
+        ],
+        description: 'Type of data to fetch'
     }),
-    Agencies: Type.Array(Type.Object({
-        id: Type.String({ description: 'The agency ID' }),
-        name: Type.String({ description: 'The agency name' })
-    })),
+    APIKey: Type.String({
+        description: 'API Key for webhook permissions'
+    }),
     'DEBUG': Type.Boolean({
         default: false,
         description: 'Print results in logs'
@@ -107,7 +103,7 @@ const OutputIncident = Type.Object({
 export default class Task extends ETL {
     static name = 'etl-jeffcom'
     static flow = [ DataFlowType.Incoming ];
-    static invocation = [ InvocationType.Schedule ];
+    static invocation = [ InvocationType.Schedule, InvocationType.Webhook ];
 
     async schema(
         type: SchemaType = SchemaType.Input,
@@ -132,149 +128,53 @@ export default class Task extends ETL {
         }
     }
 
+    static async webhooks(
+        schema: Schema,
+        task: Task
+    ): Promise<void> {
+        const env = await task.env(InputSchema);
+
+        schema.post('/:webhookid', {
+            name: 'Incoming Webhook',
+            group: 'Default',
+            description: 'Get an Everywhere Hub InReach Update',
+            params: Type.Object({
+                webhookid: Type.String()
+            }),
+            body: Type.Any(),
+            res: Type.Object({
+                status: Type.Number(),
+                message: Type.String()
+            })
+        }, async (req: any, res: any) => {
+            if (env.DEBUG) {
+                console.error(`DEBUG Webhook: ${req.params.webhookid} - ${JSON.stringify(req.body, null, 4)}`);
+            }
+
+            res.json({
+                status: 200,
+                message: 'Received'
+            });
+        });
+    }
+
     async control(): Promise<void> {
-        const env = await this.env(InputSchema);
-
-        const errors: Error[] = [];
-        const features: Static<typeof Feature.InputFeature>[] = [];
-
-        if (env.DataType === 'incidents') {
-            for (const agency of env.Agencies) {
-                try {
-                    const res = await fetch(`${env.API_URL}/v1/GetActiveIncidentsByJurisdiction`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'x-api-key': env.API_Token,
-                        },
-                        body: JSON.stringify({
-                            JurisdictionCodes: [ agency.id]
-                        })
-                    });
-
-                    if (!res.ok) {
-                        throw new Error(`Failed to fetch incidents: ${res.status} ${res.statusText}`);
-                    }
-
-                    const incidents = await res.typed(Type.Object({
-                        Success: Type.Boolean(),
-                        Error: Type.Optional(Type.String()),
-                        Incidents: Type.Union([
-                            Type.Null(),
-                            Type.Array(OutputIncident)
-                        ])
-                    }), {
-                        verbose: env.DEBUG || !!process.env.DEBUG
-                    })
-
-                    if (!incidents.Success) {
-                        console.error('Error', incidents);
-                        errors.push(new Error(`API Error for agency ${agency.name} (${agency.id}): ${incidents.Error || 'Unknown error'}`));
-                    }
-
-                    for (const incident of incidents.Incidents || []) {
-                        if (incident.LocationInformation.Latitude && incident.LocationInformation.Longitude) {
-                            const feature: Static<typeof Feature.InputFeature> = {
-                                id: String(incident.IncidentId),
-                                type: 'Feature',
-                                properties: {
-                                    type: 'a-f-G',
-                                    how: 'm-g',
-                                    callsign: incident.IncidentType.Incident_Type || 'Unknown Incident',
-                                    remarks: ''
-                                },
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: [
-                                        incident.LocationInformation.Longitude,
-                                        incident.LocationInformation.Latitude,
-                                    ]
-                                }
-                            };
-
-                            features.push(feature);
-                        }
-                    }
-                } catch (err) {
-                    errors.push(new Error(`Error processing Incident: ${agency.name} (${agency.id}): ${(err as Error).message}`));
-                }
-            }
-        } else if (env.DataType === 'units') {
-            for (const agency of env.Agencies) {
-                try {
-                    const res = await fetch(`${env.API_URL}/v1/GetActiveUnitsByJurisdiction`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'x-api-key': env.API_Token,
-                        },
-                        body: JSON.stringify({
-                            JurisdictionCodes: [ agency.id ]
-                        })
-                    });
-
-                    const units = await res.typed(Type.Object({
-                        Success: Type.Boolean(),
-                        Error: Type.Optional(Type.String()),
-                        Units: Type.Union([Type.Null(), Type.Array(OutputUnit)])
-                    }), {
-                        verbose: env.DEBUG || !!process.env.DEBUG
-                    })
-
-                    if (!units.Success) {
-                        console.error('Error', units);
-                        errors.push(new Error(`API Error for agency ${agency.name} (${agency.id}): ${units.Error || 'Unknown error'}`));
-                    }
-
-                    if (units.Units === null) {
-                        continue;
-                    }
-
-                    for (const unit of units.Units) {
-                        const feature: Static<typeof Feature.InputFeature> = {
-                            id: String(unit.UnitID),
-                            type: 'Feature',
-                            properties: {
-                                type: 'a-f-G',
-                                how: 'm-g',
-                                callsign: unit.UnitName,
-                                remarks: unit.StatusName
-                            },
-                            geometry: {
-                                type: 'Point',
-                                coordinates: [
-                                    unit.Longitude,
-                                    unit.Latitude,
-                                ]
-                            }
-                        };
-
-                        features.push(feature);
-                    }
-                } catch (err) {
-                    errors.push(new Error(`Error processing Unit: ${agency.name} (${agency.id}): ${(err as Error).message}`));
-                }
-            }
-        } else {
-            throw new Error(`Unsupported DataType: ${env.DataType}`);
-        }
-
-
         const fc: Static<typeof Feature.InputFeatureCollection> = {
             type: 'FeatureCollection',
-            features: features
+            features: []
         }
 
         await this.submit(fc);
-
-        if (errors.length > 0) {
-            throw new Error(`Encountered ${errors.length} errors:\n` + errors.map(e => e.message).join('\n'));
-        }
     }
 }
 
 await local(await Task.init(import.meta.url), import.meta.url);
-export async function handler(event: Event = {}) {
-    return await internal(new Task(import.meta.url), event);
-}
 
+export async function handler(event: Event = {}, context?: object) {
+    return await internal(await Task.init(import.meta.url, {
+        logging: {
+            event: process.env.DEBUG ? true : false,
+            webhooks: true
+        }
+    }), event, context);
+}
